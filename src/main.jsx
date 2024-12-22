d7d3947bd19da3844d72d6676ee9f75fa004832b
Adebayo Sunday Samuel <89652234+Sarmuel001@users.noreply.github.com>
2024-12-22 23:47:36 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export const element = <FontAwesomeIcon icon="fas fa-shopping-cart" />
 
