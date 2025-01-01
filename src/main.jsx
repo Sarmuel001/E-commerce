192e31024ffc7e1e65b99668ca42dacfc8c6cf1c
sarmuel001 <adebayosunday51@gmail.com>
2025-01-01 03:24:17 +0100

    new update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-export const element = <FontAwesomeIcon icon="fas fa-shopping-cart" />
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
